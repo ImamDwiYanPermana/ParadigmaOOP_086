574a486634f5657c8d73bef92c08328661a1421b
ImamDwiYanPermana <imampermana224@gmail.com>
2023-05-24 14:03:54 +0700

    Memanggil dan mencetak sama men delete

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -58,9 +58,25 @@ class dokter;
 int main()
 {
     dokter* varDokter1 = new dokter("dr.budi");
-    dokter* varDokter1 = new dokter("dr.Tono");
-    dokter* varDokter1 = new dokter("Andii");
-    dokter* varDokter1 = new dokter("Lia");
+    dokter* varDokter2 = new dokter("dr.Tono");
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("jarji");
+
+    varDokter1->tambahPasien(varPasien1);
+    varDokter1->tambahPasien(varPasien2);
+    varDokter2->tambahPasien(varPasien1);
+
+    varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+    return 0;
+
 }
 
 
