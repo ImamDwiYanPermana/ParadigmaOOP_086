4b6f274c0fe3540683f58d7254201d7d389f2057
ImamDwiYanPermana <imampermana224@gmail.com>
2023-05-24 11:20:29 +0700

    Mengambil classs pelajar atau member dari varaible orang dan menjadi publik serta mengdeklarasikan pelajar

--- ParadigmaOOP/ParadigmaOOP.cpp
+++ ParadigmaOOP/ParadigmaOOP.cpp
@@ -21,8 +21,17 @@ public:
     }
 };
 
+class pelajar : public orang {
+public : 
+    string sekolah;
 
-
+    pelajar (string pNama, string pSekolah):
+        orang (pNama),
+        sekolah(pSekolah) {
+        cout << " pelajar dohapus\n" << endl;
+    }
+    
+};
 int main()
 {
     std::cout << "Hello World!\n";
