eb1d15f3e64c9d6c51e11801e9e37eb71faa984d
ImamDwiYanPermana <imampermana224@gmail.com>
2023-05-24 14:50:54 +0700

    memamnggil manusai menggunakan pointing dan varmanusia

---
+++ mainJM/mainJM.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+#include "jantung.h"
+#include "manusia.h"
+
+int main()
+{
+    manusia* varManusia = new manusia("jono");
+    delete varManusia;
+    return 0;
+}
+
+
