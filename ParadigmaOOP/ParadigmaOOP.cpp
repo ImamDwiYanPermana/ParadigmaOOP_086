23e8e5f90bbd61b53de844861f81dd46e097d005
ImamDwiYanPermana <imampermana224@gmail.com>
2023-05-24 11:05:57 +0700

    Add project files.

---
+++ ParadigmaOOP/ParadigmaOOP.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+clas ornag{
+public :
+    string Nama;
+
+    orang(string pNama);
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
